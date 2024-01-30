4fb176d5987e06fe41b7f10f53df54bec180f7fe
Artur Rego-Costa <arturregocosta@gmail.com>
2024-01-30 14:17:35 -0500

    Sorted displayed items and split them based on station

--- display_times.py
+++ display_times.py
@@ -20,31 +20,58 @@ min_wait_time = -2*60 # minimum wait time to display
 max_wait_time = 20*60 # maximum wait time to display
 
 # ANSI escape code for yellow
-ORANGE = "\033[38;5;214m"
+RED = "\033[1;31m"
 YELLOW = "\033[93m"
 GREEN = "\033[1;32m"
 RESET = "\033[0m"
+STATION = "\033[1;30m\033[1;47m"
 col_dict = {
 	"R" : YELLOW,
-	"F" : ORANGE,
+	"F" : RED,
 	"G" : GREEN
 }
 
+# Order wait times based on wait time
+wait_times.sort(key = lambda x: x['wait_time'])
+
+# Split wait times based on station
+wait_times_split = {}
+for wait_time in wait_times:
+	if wait_time['stop_id'] in wait_times_split:
+		wait_times_split[wait_time['stop_id']] += [wait_time]
+	else:
+		wait_times_split[wait_time['stop_id']] = [wait_time]
+
 # Go through wait times and print them nicely
 current_time = int(time.time())
-for x in wait_times:
-	cur_time_to_arrival = (x['wait_time'] + x['recorded_time']) - current_time
 
-	if cur_time_to_arrival < min_wait_time or cur_time_to_arrival > max_wait_time:
-		continue
+for stop_id, cur_wait_times in wait_times_split.items():
+	print(f"{STATION}{stop_names[stop_id]}{RESET}")
 
-	min_display = f"{(cur_time_to_arrival//60):2d}"
-	sec_remainder = cur_time_to_arrival%60
-	sec_display = f"{(15*(sec_remainder//15)):02d}"
+	for x in cur_wait_times:
+		wait_time = (x['wait_time'] + x['recorded_time']) - current_time
 
-	if min_display == 0 and sec_display == 0:
-		print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : arriving")
-	elif sec_display == 0:
-		print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : {min_display}min")
-	else:
-		print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : {min_display}min{sec_display}")
+		if wait_time < min_wait_time or wait_time > max_wait_time:
+			continue
+
+		neg_wait_time = False
+		if wait_time < 0:
+			neg_wait_time = True
+			wait_time = -wait_time
+
+		min_num = wait_time//60
+		sec_num = 15*((wait_time%60)//15)
+
+		if neg_wait_time:
+			min_display = f"-{min_num:1d}"
+		else:
+			min_display = f"{min_num:2d}"
+
+		sec_display = f"{sec_num:02d}"
+
+		if min_num == 0 and sec_num == 0:
+			print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : arriving")
+		elif sec_num == 0:
+			print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : {min_display}min")
+		else:
+			print(f"{col_dict[x['train_id']]}({x['train_id']}){RESET} : {min_display}min{sec_display}")
